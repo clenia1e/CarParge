e135103ea541bad0a22fa6466cb81cb5f8f945e7
Clenia Guedes <cleniaguedes@MacBook-Pro-de-Clenia.local>
2021-08-07 22:12:08 -0300

    First commit

---
+++ index.js
@@ -0,0 +1,81 @@
+
+
+
+const selectedAuto = () => {
+    var selectTitle = document.getElementById("title")
+    let select = document.getElementById("select-cars")
+    let selectText = select.options[select.selectedIndex].text
+    let selectYears = document.getElementById("select-years").value
+    var selectValue = document.getElementById("select-cars").value
+    var resultImg = document.getElementById("result-img")
+    const selectButton = document.getElementById("select-button")
+
+    var img = document.getElementById('Auto')
+
+    console.log('o select', selectValue)
+    if (selectValue == "230000") {
+        console.log(img)
+        // window.alert('voce selecionou compass')
+        resultImg.innerHTML = `<img id='Auto' src='img/compass.png' />`
+
+    } else if (selectValue == "400000") {
+        // window.alert('voce selecionou grand')
+        resultImg.innerHTML = `<img id='Auto' src='img/grand.png' />`
+    } else {
+        //window.alert('voce selecionou renegade')
+        resultImg.innerHTML = `<img id='Auto' src='img/renegade.png' />`
+    }
+    selectTitle.innerHTML = ` <h1 class="car-title">  ${selectText} ${selectYears} </h1>`
+
+}
+
+const simulate = () => {
+    let select = document.getElementById("select-cars")
+    let selectValue = select.value
+    let selectYears = document.getElementById("select-years").value
+    let paymentValue = document.getElementById("payment").value
+    let resultPrice = document.getElementById("result-price")
+    let resultYears
+    let totalPayment
+    console.log(selectYears)
+
+    switch (selectYears) {
+
+        case "2022":
+            resultYears = Number(selectValue) + 10000
+            break;
+
+        case "2021":
+            resultYears = Number(selectValue) + 5000
+            break;
+
+        case "2020":
+            resultYears = Number(selectValue) - 10000
+            break;
+    }
+    switch (paymentValue) {
+
+        case "1":
+            totalPayment = resultYears
+            break;
+
+        case "2":
+            totalPayment = resultYears + 80000
+            break;
+    }
+    console.log(totalPayment)
+    resultPrice.innerHTML = ` <p class="total-style">   Total:  </p>  <p class="price-style"> R$ ${totalPayment},00  </p>`
+}
+
+const buy = () => {
+    let select = document.getElementById("select-cars")
+    const selectText = select.options[select.selectedIndex].text
+    let selectValue = select.value
+    let selectYearsValue = document.getElementById("select-years").value
+    let paymentValue = document.getElementById("payment").value
+    let purchaseFinish = document.getElementById("purchase-finish")
+
+    window.alert(` You bought ${selectText} ${selectYearsValue} `)
+    //purchaseFinish.innerHTML = `<p> Auto: ${selectText}</p> <p> Year ${selectYearsValue}</p> <p> Total Value ${selectValue}</p>`
+
+}
